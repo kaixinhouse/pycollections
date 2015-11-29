d829f9e5256a364717b0a9d94a1a6df54de90e84
kaixinhouse <kaixinhouse@gmail.com>
2015-11-29 14:20:05 +0800

    add git ignore file

--- utils/checkers.py
+++ utils/checkers.py
@@ -9,3 +9,8 @@ def is_valid_md5(md5):
     if any([c not in '0123456789abcdef' for c in md5]):
         return False
     return True
+
+
+if __name__ == '__main__':
+    print is_valid_md5('12345')
+    print is_valid_md5('37adc72339a0c2c755e7fef346906330')
